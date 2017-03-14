bcb6cb5034786f295a6962dab901c4808cd56b13
felipe <fsanchez@undanet.com>
2017-03-14 15:55:17 +0100

    config views and forms

--- src/AppBundle/Admin/UserAdmin.php
+++ src/AppBundle/Admin/UserAdmin.php
@@ -10,16 +10,51 @@ class UserAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper->add('username', 'text');
+        $formMapper
+            ->with('Información Personal')
+                ->add('username', 'text')
+                ->add('email','email')
+            ->end()
+            ->with('Configuración')
+                ->add('roles', 'choice', array(
+                    'choices' => array(
+                        'ROLE_ADMIN' => 'Admin',
+                        'ROLE_USER' => 'User'
+                    ),
+                    'expanded' => false,
+                    'multiple' => true,
+                    'required' => false
+                ))
+            ->end()
+        ;
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-        $datagridMapper->add('username');
+        $datagridMapper
+            ->add('username')
+            ->add('email')
+            ->add('enabled')
+            //->add('last_login')
+            //->add('password_requested_at')
+            ->add('roles')
+        ;
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('username');
+        $listMapper
+            ->addIdentifier('username')
+            ->add('email')
+            ->add('enabled')
+            ->add('roles')
+        ;
+    }
+
+    public function toString($object)
+    {
+        return $object instanceof User
+            ? $object->getUsername()
+            : 'Usuario'; // shown in the breadcrumb on the create view
     }
 }
